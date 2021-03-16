c15ae148de9b110cf3b0406c4220f3623cdb1707
learn_and_think <ilovesp20@outlook.com>
2021-03-16 08:54:22 +0800

    '学习尚医通'

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -0,0 +1,32 @@
+package com.atguigu.yygh.hosp.controller;
+
+import com.atguigu.yygh.hosp.service.HospitalSetService;
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/hosp/hospitalSet")
+public class HospitalSetController {
+
+    @Autowired
+    private HospitalSetService hospitalSetService;
+
+    //1.查询医院设置表中所有的信息
+    @GetMapping("findAll")
+    public List<HospitalSet> findAllHospitalSet(){
+        List<HospitalSet> list = hospitalSetService.list();
+        return list;
+    }
+
+    //2.逻辑删除医院信息
+    @GetMapping("delete/{id}")
+    public boolean removeHospitalSetById(@PathVariable Long id) {
+        boolean flag = hospitalSetService.removeById(id);
+        return flag;
+    }
+
+
+}
