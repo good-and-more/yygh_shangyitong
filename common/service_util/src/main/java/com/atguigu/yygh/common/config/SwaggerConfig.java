f0c227b84b4de7f7a145f47c6fc1244afda2a990
learn_and_think <ilovesp20@outlook.com>
2021-03-17 23:34:37 +0800

    整合swagger3

--- common/service_util/src/main/java/com/atguigu/yygh/common/config/SwaggerConfig.java
+++ common/service_util/src/main/java/com/atguigu/yygh/common/config/SwaggerConfig.java
@@ -1,8 +1,44 @@
 package com.atguigu.yygh.common.config;
 
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableOpenApi
 public class SwaggerConfig {
+    private static final String VERSION = "1.0.0";
+    /**
+     * 创建API
+     */
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.OAS_30)
+                .enable(true)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 添加摘要信息
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("service_hosp医院信息管理")
+                .contact(new Contact("learn_and_think", "https://github.com/good-and-more/yygh_shangyitong", "yygh@atguigu.com"))
+                .description("尚医通-接口文档测试")
+                .version(VERSION)
+                .build();
+    }
 }
--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -2,6 +2,8 @@ package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,12 +11,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
+@Api(tags = "医院信息管理 HospitalSetController")
 public class HospitalSetController {
 
     @Autowired
     private HospitalSetService hospitalSetService;
 
     //1.查询医院设置表中所有的信息
+    @ApiOperation("查询所有医院信息")
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet(){
         List<HospitalSet> list = hospitalSetService.list();
@@ -22,6 +26,7 @@ public class HospitalSetController {
     }
 
     //2.逻辑删除医院信息
+    @ApiOperation("根据id逻辑删除医院")
     @GetMapping("delete/{id}")
     public boolean removeHospitalSetById(@PathVariable Long id) {
         boolean flag = hospitalSetService.removeById(id);
