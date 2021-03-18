88622c650ceae61123159e4e20fa46a4e1446307
learn_and_think <ilovesp20@outlook.com>
2021-03-18 23:38:29 +0800

    '不成功的分页类'

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.atguigu")
+@ComponentScan(basePackages = "com.atguigu")//加这个有什么用？
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,10 +1,15 @@
 package com.atguigu.yygh.hosp.controller;
 
+import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,18 +25,71 @@ public class HospitalSetController {
     //1.查询医院设置表中所有的信息
     @ApiOperation("查询所有医院信息")
     @GetMapping("findAll")
-    public List<HospitalSet> findAllHospitalSet(){
+    public Result findAllHospitalSet(){
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok(list);
     }
 
     //2.逻辑删除医院信息
     @ApiOperation("根据id逻辑删除医院")
     @GetMapping("delete/{id}")
-    public boolean removeHospitalSetById(@PathVariable Long id) {
+    public Result removeHospitalSetById(@PathVariable Long id) {
         boolean flag = hospitalSetService.removeById(id);
-        return flag;
+        return flag ? Result.ok(flag) : Result.fail(flag);
     }
+
+
+//    增
+//    public Result addHospitalSet(HospitalSet hospitalSet){
+//        return null;
+//    }
+
+//    批量删除
+
+//    根据条件分页查询
+    @ApiOperation("根据条件分页查询医院")
+    @PostMapping("selectPage")
+    public Result selectPageHospitalSet(Integer currentPage,
+                                        Integer pageSize,
+                                        @RequestBody HospitalSetQueryVo hospitalSetQueryVo) {
+        Page<HospitalSet> page = new Page<>(currentPage, pageSize);
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(hospitalSetQueryVo.getHosname())) {
+            wrapper.like("hosname", hospitalSetQueryVo.getHosname());
+        }
+        if(!StringUtils.isEmpty(hospitalSetQueryVo.getHoscode())) {
+            wrapper.eq("hoscode", hospitalSetQueryVo.getHoscode());
+        }
+        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, wrapper);
+        return Result.ok(pageHospitalSet);
+    }
+
+    //3 条件查询带分页
+    @ApiOperation("教程版根据条件分页查询医院")
+    @PostMapping("findPageHospSet/{current}/{limit}")
+    public Result findPageHospSet(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody
+                                          (required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+        //创建page对象，传递当前页，每页记录数
+        Page<HospitalSet> page = new Page<>(current,limit);
+        //构建条件
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+        String hosname = hospitalSetQueryVo.getHosname();//医院名称
+        String hoscode = hospitalSetQueryVo.getHoscode();//医院编号
+        if(!StringUtils.isEmpty(hosname)) {
+            wrapper.like("hosname",hospitalSetQueryVo.getHosname());
+        }
+        if(!StringUtils.isEmpty(hoscode)) {
+            wrapper.eq("hoscode",hospitalSetQueryVo.getHoscode());
+        }
+        //调用方法实现分页查询
+        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, wrapper);
+        //返回结果
+        return Result.ok(pageHospitalSet);
+    }
+
+//    根据条件更新
 
 
 }
